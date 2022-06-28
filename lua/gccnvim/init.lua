5d9a23f9bcf4ddbcea9d4bc71de768ed1ee9fb8b
gcdcintra <gustavocdcintra@gmail.com>
2022-06-28 04:14:43 -0300

    ahoooi

---
+++ lua/gccnvim/init.lua
@@ -0,0 +1,42 @@
+local augroup = vim.api.nvim_create_augroup
+auGroup = augroup('gccnvimGroup', {})
+
+require("gccnvim.cmp-use-rust")
+require("gccnvim.set")
+require("gccnvim.plugins")
+require("gccnvim.neogit")
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = auGroup,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = auGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+
+-- Debug options?
